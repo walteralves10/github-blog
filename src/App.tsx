2bae1006e615ce10ccaa090b99789dfdab3a000d
walteralves10 <walter.rezende@simpleagro.com.br>
2024-05-14 09:48:43 -0300

    feat: use Provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,19 @@ import { defaultTheme } from './styles/themes/default'
 import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
+import { UsersProvider } from './contexts/users-context'
+import { IssuesProvider } from './contexts/issues-context'
 
 function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
       <BrowserRouter>
-        <Router />
+        <UsersProvider>
+          <IssuesProvider>
+            <Router />
+          </IssuesProvider>
+        </UsersProvider>
       </BrowserRouter>
     </ThemeProvider>
   )
